1c63e16bda98a5935850ce99f676390d50a45f0c
razz97 <5alexbouoliete@gmail.com>
2019-11-17 16:38:25 +0100

    toggle params and responses

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, QueryList, } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, QueryList, } from '@angular/core';
 import { Path } from '../model/Path'
 import { Operation, Method } from '../model/Operation';
 import { Param, ParamLocation } from '../model/Param';
 import { DocumentService } from '../common/document.service';
 import { ContentType, Response } from '../model/Response';
-import { SchemaType } from '../model/Schema';
 import { SchemaformComponent } from '../schemaform/schemaform.component';
 
 declare const Redoc: any;
@@ -27,12 +26,22 @@ export class EditorComponent implements OnInit {
   public paramLocations: ParamLocation[] = ["Path", "Query", "Header", "Cookie"];
   public contentTypes: ContentType[] = ["application/json", "text/plain", "text/html", "application/xml"];
 
+  public collapsed = {
+    params: false,
+    responses: false
+  }
+
   public methodSelected: Method = this.methods[0];
 
   public path: Path = new Path();
 
   public addParam(params: Param[]) {
+    this.collapsed.params = false;
     params.push(new Param());
+  }
+
+  public toggleCollapse(key: string) {
+    this.collapsed[key] = !this.collapsed[key];
   }
 
   public removeParam(params: Param[], param: Param) {
@@ -63,7 +72,7 @@ export class EditorComponent implements OnInit {
   }
 
   public refresh() {
-    Redoc.init(this.documentService.buildDocument(this.path).toJSON(), {}, this.redoc.nativeElement);
+    Redoc.init(this.documentService.buildDocument(this.path).toJSON(), {pathInMiddlePanel: true}, this.redoc.nativeElement);
   }
 
 
