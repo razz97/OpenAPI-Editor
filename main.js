c57887ae9eccfcabf3ff4667fae9391b1c3a50bd
razz97 <5alexbouoliete@gmail.com>
2019-11-17 04:00:48 +0100

    allowed dev tools

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const { app, BrowserWindow, Menu, screen } = require('electron')
 let win;
 Menu.setApplicationMenu(null);
 
-function createWindow () {
+function createWindow() {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize
   // Create the browser window.
   win = new BrowserWindow({
-    width: width / 1.5, 
-    height: height / 1.5,
+    width: width / 2,
+    height: height / 2,
     backgroundColor: '#ffffff',
     icon: `file://${__dirname}/dist/assets/logo.png`
   })
@@ -16,6 +16,10 @@ function createWindow () {
   // Allow user to resize window
   win.resizable = true;
 
+  // Open the DevTools.
+  win.webContents.openDevTools();
+
+  // Load angular
   win.loadURL(`file://${__dirname}/dist/openapi-generator/index.html`)
 
   // Event when the window is closed.
@@ -23,6 +27,8 @@ function createWindow () {
     win = null
   })
 }
+
+
 
 // Create window on electron intialization
 app.on('ready', createWindow)
