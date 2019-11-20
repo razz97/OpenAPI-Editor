32822b4a12f651c1564daeb76c7b71ef42cd44ad
razz97 <5alexbouoliete@gmail.com>
2019-11-21 00:57:09 +0100

    refactor components

--- src/app/components/path/path.component.ts
+++ src/app/components/path/path.component.ts
@@ -3,6 +3,10 @@ import { Path } from '../../model/Path';
 import { Operation, Method } from '../../model/Operation';
 import { DocumentService } from '../../services/document.service';
 import { Remote } from 'electron';
+import { Param } from 'src/app/model/Param';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
+import { Response } from 'src/app/model/Response';
 
 declare const Redoc: any;
 
@@ -10,52 +14,33 @@ declare const Redoc: any;
   selector: 'app-editor',
   templateUrl: './path.component.html'
 })
-export class PathComponent implements OnInit {
-
-  remote: Remote;
-
+export class PathComponent {
+  
   constructor(
     private documentService: DocumentService,
+    private router: Router,
+    private dataService: DataService
   ) {
     this.remote = (<any>window).require('electron').remote;
   }
 
-  ngOnInit() {
-  }
-
   @ViewChild('redoc', { static: true }) redoc: ElementRef;
 
-  public methods: Method[] = ["GET", "POST", "PUT", "DELETE"];
+  remote: Remote;
 
-  public methodSelected: Method = this.methods[0];
+  methods: Method[] = ["GET", "POST", "PUT", "DELETE"];
 
-  public path: Path = new Path();
+  methodSelected: Method = this.methods[0];
 
-  public removeOperation(operation: Operation) {
+  path: Path = new Path();
+
+  removeOperation(operation: Operation) {
     this.methods.push(operation.method);
     if (!this.methodSelected) {
       this.methodSelected = operation.method;
     }
     this.path.operations.splice(this.path.operations.indexOf(operation), 1);
   }
-
-  public addOperation() {
-    const tmp = this.methodSelected;
-    this.methods.splice(this.methods.indexOf(this.methodSelected), 1);
-    this.methodSelected = this.methods[0];
-    this.path.operations.push(new Operation(tmp));
-  }
-
-  public refresh() {
-    Redoc.init(
-      this.documentService.buildDocument(this.path).toJSON(),
-      {
-        pathInMiddlePanel: true,
-        hideDownloadButton: true
-      },
-      this.redoc.nativeElement);
-  }
-
 
   save() {
     const folder = this.remote.dialog.showSaveDialogSync({});
@@ -64,5 +49,47 @@ export class PathComponent implements OnInit {
     }
   }
 
+  addParam(params: Param[]) {
+    params.push(new Param());
+  }
+
+  removeParam(params: Param[], param: Param) {
+    params.splice(params.indexOf(param), 1);
+  }
+
+  editParam(param: Param) {
+    this.router.navigateByUrl('param');
+    this.dataService.sendParam(param);
+  }
+
+  addResponse(responses: Response[]) {
+    responses.push(new Response());
+  }
+
+  removeResponse(responses: Response[],response: Response) {
+    responses.splice(responses.indexOf(response), 1);
+  }
+
+  editResponse(response: Response) {
+    this.router.navigateByUrl('response');
+    this.dataService.sendResponse(response);
+  }
+
+  addOperation() {
+    const tmp = this.methodSelected;
+    this.methods.splice(this.methods.indexOf(this.methodSelected), 1);
+    this.methodSelected = this.methods[0];
+    this.path.operations.push(new Operation(tmp));
+  }
+
+  refresh() {
+    Redoc.init(
+      this.documentService.buildDocument(this.path).toJSON(),
+      {
+        pathInMiddlePanel: true,
+        hideDownloadButton: true
+      },
+      this.redoc.nativeElement);
+  }
 
 }
